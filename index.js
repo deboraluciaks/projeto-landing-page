8b1a8ac9f08cd8a530a6c98b12421390e2a1a428
Débora Lucia Koppe Soares <deboraluciak@hotmail.com>
2023-04-28 10:44:46 -0300

    projeto quase finalizado

---
+++ index.js
@@ -0,0 +1,49 @@
+let botãoBanner = document.getElementById("botão-banner");
+
+
+function botãoScroll () {
+    
+}
+
+
+
+
+
+
+
+/* CÓDIGO ANTIGO   
+percebi que por mais que esse código funcionasse, ele precisava do eventListener para a mensagem não aparecer caso os campos estivessem vazios.
+
+
+function botaoFormEmail(event){
+    event.preventDefault()
+    let nomeVar = document.getElementById("nome")
+    let emailVar = document.getElementById("email")
+    let numVar = document.getElementById("celular")
+    
+
+    document.getElementById("resposta-usuario").innerHTML = `Entraremos em contato através do endereço ${emailVar.value}`
+
+
+    nomeVar.value = ""
+    emailVar.value = ""
+    numVar.value = ""
+
+} */
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit" ,function(event){
+    event.preventDefault()
+    let nomeVar = document.getElementById("nome")
+    let emailVar = document.getElementById("email")
+    let numVar = document.getElementById("celular")
+    
+
+    document.getElementById("resposta-usuario").innerHTML = `Entraremos em contato através do endereço ${emailVar.value}`
+
+    nomeVar.value = ""
+    emailVar.value = ""
+    numVar.value = ""
+
+})
